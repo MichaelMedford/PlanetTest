c3af38e8e1caccf5c0a469ab6bcaf6bcb0206181
Michael Medford <MichaelMedford@berkeley.edu>
2020-09-27 15:17:12 -0700

    ndvi calculation

--- product.py
+++ product.py
@@ -1,12 +1,13 @@
 import os
 import rasterio
 import json
-import xml.etree.ElementTree as ET
+from xml.dom import minidom
 import matplotlib.pyplot as plt
 
 
 class Scene(object):
     def __init__(self, filename):
+        # parse filenames
         self.filename_base = self.parse_filename_base(filename)
         self.filename_scene = f'{self.filename_base}_3B_AnalyticMS_clip.tif'
         self.filename_metadata_xml = f'{self.filename_base}_3B_' \
@@ -14,11 +15,14 @@ class Scene(object):
         self.filename_metadata_json = f'{self.filename_base}_metadata.json'
         self.filename_udm2 = f'{self.filename_base}_3B_udm2_clip.tif'
 
+        # check for files
         self.check_for_files()
 
-        self.scene_bands = ['blue', 'green', 'red', 'nir']
-        self.udm2_bands = ['clear', 'snow', 'shadow', 'light_haze',
-                           'heavy_haze', 'cloud', 'confidence', 'unusable']
+        # load files into memory
+        self.scene_bands = {'blue': 1, 'green': 2, 'red': 3, 'nir': 4}
+        self.udm2_bands = {'clear': 1, 'snow': 2, 'shadow': 3,
+                           'light_haze': 4, 'heavy_haze': 5, 'cloud': 6,
+                           'confidence': 7, 'unusable': 8}
         self.scenes = self.load_scenes()
         self.udm2 = self.load_udm2()
         self.metadata_xml = self.load_metadata_xml()
@@ -51,13 +55,32 @@ class Scene(object):
                                   self.udm2_bands, 'udm2')
 
     def load_metadata_xml(self):
-        tree = ET.parse(self.filename_metadata_xml)
-        return tree.getroot()
+        return minidom.parse(self.filename_metadata_xml)
 
     def load_metadata_json(self):
         with open(self.filename_metadata_json) as f:
             metadata_json = json.load(f)
         return metadata_json
+
+    def _load_toa_reflectance_coeff(self, band_idx):
+        nodes = self.metadata_xml.getElementsByTagName("ps:bandSpecificMetadata")
+        coeff = nodes[band_idx].getElementsByTagName("ps:reflectanceCoefficient")[0].firstChild.data
+        return float(coeff)
+
+    def _load_scene_toa_reflectance(self, scene_band):
+        band = self.scenes[scene_band]
+        band_idx = self.scene_bands[scene_band] - 1
+        coeff = self._load_toa_reflectance_coeff(band_idx)
+        return band * coeff
+
+    def calculate_ndvi(self, convert_to_toa=True):
+        if convert_to_toa:
+            nir = self._load_scene_toa_reflectance('nir')
+            red = self._load_scene_toa_reflectance('red')
+        else:
+            nir = self.scenes['nir']
+            red = self.scenes['red']
+        return (nir - red) / (nir + red)
 
     def _plot_bands(self, fig, ax, bands, labels, ext):
         fig.suptitle(self.filename_base + f' {ext}')
@@ -79,3 +102,4 @@ class Scene(object):
     def plot_udm2(self):
         fig, ax = plt.subplots(4, 2, figsize=(8, 12))
         self._plot_bands(fig, ax, self.udm2, self.udm2_bands, 'udm2')
+
