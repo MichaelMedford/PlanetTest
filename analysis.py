5b7f3da67d28241b9e5529d801826a477129a2a9
Michael Medford <MichaelMedford@berkeley.edu>
2020-09-27 20:03:00 -0700

    plot water_masks

--- analysis.py
+++ analysis.py
@@ -16,7 +16,7 @@ from product import Scene
 
 
 def load_scenes(remove_first_image=False):
-    print('Loading scenes...')
+    print('\nLoading scenes...')
     fname_arr = glob.glob('data/*AnalyticMS_clip.tif')
     fname_arr.sort()
 
@@ -34,18 +34,24 @@ def load_scenes(remove_first_image=False):
                 scene = Scene(fname, row_clip=15)
         scene_arr.append(scene)
 
-    print('\n')
     return scene_arr
 
 
-def calculate_mask(scene_arr):
+def plot_images_and_masks(scene_arr):
+    print('\nPlotting images and masks...')
+    for scene in scene_arr:
+        scene.plot_images()
+        scene.plot_udm2()
+
+
+def calculate_stack_mask(scene_arr):
     mask_arr = [s.udm2['clear'].astype(bool) for s in scene_arr]
-    mask = np.logical_and.reduce(mask_arr)
-    return mask
+    stack_mask = np.logical_and.reduce(mask_arr)
+    return stack_mask
 
 
 def calculate_ndvi_stats(scene_arr, water_mask_percentile=50):
-    mask_stack = calculate_mask(scene_arr)
+    mask_stack = calculate_stack_mask(scene_arr)
 
     ndvi_med_arr = []
     ndvi_std_arr = []
@@ -74,14 +80,6 @@ def generate_delta_days(acquired_arr):
     return np.array(delta_days_arr)
 
 
-def plot_iamges_and_masks(scene_arr):
-    print('Plotting images and masks...')
-    for scene in scene_arr:
-        scene.plot_images()
-        scene.plot_udm2()
-    print('\n')
-
-
 def plot_ndvi_trends(scene_arr, remove_first_image=False,
                      water_mask_percentile=50):
     ndvi_stats = calculate_ndvi_stats(scene_arr,
@@ -93,7 +91,8 @@ def plot_ndvi_trends(scene_arr, remove_first_image=False,
     ndvi_rate_of_change = (ndvi_med_arr[1:] - ndvi_med_arr[
                                               :-1]) / delta_days_arr
     fig, ax = plt.subplots(2, 1, figsize=(12, 6))
-    fig.suptitle('Presence of Green Vegetation')
+    fig.suptitle(f'Presence of Green Vegetation | '
+                 f'{water_mask_percentile} Precentile Water Mask')
     ax[0].errorbar(acquired_arr, ndvi_med_arr, yerr=ndvi_std_arr)
     ax[0].set_ylabel('NDVI')
     ax[0].set_ylim(.4, .9)
@@ -122,9 +121,45 @@ def plot_ndvi_trends(scene_arr, remove_first_image=False,
     print(f'{fname} saved')
 
 
-def analyze_scenes(remove_first_image, water_mask_percentile):
+def plot_water_mask(scene_arr):
+    ndvi_stats = calculate_ndvi_stats(scene_arr, water_mask_percentile=100)
+    acquired_arr, ndvi_med_arr_raw, ndvi_std_arr_raw = ndvi_stats
+    ndvi_stats = calculate_ndvi_stats(scene_arr, water_mask_percentile=50)
+    _, ndvi_med_arr_clipped, ndvi_std_arr_clipped = ndvi_stats
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    fig.suptitle('Presence of Green Vegetation')
+    ax.errorbar(acquired_arr, ndvi_med_arr_raw, yerr=ndvi_std_arr_raw,
+                color='g', label='No Water Mask', lw=5)
+    ax.errorbar(acquired_arr, ndvi_med_arr_clipped, yerr=ndvi_std_arr_clipped,
+                color='b', label='50 Percentile Water Mask', lw=5)
+    ax.set_xlabel('Observation Date')
+    ax.set_ylabel('NDVI')
+    ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+    ax.set_ylim(.4, .9)
+    for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
+                 ax.get_xticklabels() + ax.get_yticklabels()):
+        item.set_fontsize(14)
+    for label in ax.get_xticklabels():
+        label.set_rotation(20)
+    ax.legend(loc=9, fontsize=14)
+    fig.tight_layout()
+    fig.subplots_adjust(top=.9)
+
+
+    fname = 'figures/water_mask.png'
+    fig.savefig(fname, dpi=75, bbox_inches='tight', pad_inches=0.05)
+    print(f'{fname} saved')
+
+
+def analyze_scenes(remove_first_image, water_mask_percentile,
+                   skip_plot_image_masks):
     scene_arr = load_scenes(remove_first_image=remove_first_image)
-    plot_iamges_and_masks(scene_arr)
+    if not skip_plot_image_masks:
+        plot_images_and_masks(scene_arr)
+
+    print('\nPlotting analysis figures')
+    plot_water_mask(scene_arr)
     plot_ndvi_trends(scene_arr, remove_first_image=remove_first_image,
                      water_mask_percentile=water_mask_percentile)
 
@@ -134,6 +169,12 @@ def main():
                                                  'vegetation trends.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
+    help_str = 'This analysis script generates plots of the images and ' \
+               'masks for each scene. This flag skips that output.'
+    parser.add_argument('--skip-plot-images-masks',
+                        action='store_false', help=help_str)
+    parser.set_defaults(skip_plot_image_masks=True)
+
     help_str = 'A stack mask is constructed by finding the "clear" pixels ' \
                'that all scenes in the set of imaages have in common. ' \
                'The first analysis has 15 less rows than the remaining ' \
@@ -141,7 +182,7 @@ def main():
                'Activating this flag removes the first image from the ' \
                'analysis. If not selected, the remaining scenes are ' \
                'clipped by 15 rows to generate a stack mask.'
-    parser.add_argument('--remove-first-image', dest='remove_first_image',
+    parser.add_argument('--remove-first-image',
                         action='store_true', help=help_str)
     parser.set_defaults(remove_first_image=False)
 
@@ -156,7 +197,8 @@ def main():
 
     args = parser.parse_args()
 
-    analyze_scenes(args.remove_first_image, args.water_mask_percentile)
+    analyze_scenes(args.remove_first_image, args.water_mask_percentile,
+                   args.skip_plot_image_masks)
 
 
 if __name__ == '__main__':
