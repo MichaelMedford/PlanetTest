28cbe7b85e5f32826a8d4a0218f1c31e6818cb3f
Michael Medford <MichaelMedford@berkeley.edu>
2020-09-27 18:38:29 -0700

    include row clipping

--- product.py
+++ product.py
@@ -8,11 +8,11 @@ from datetime import datetime
 
 
 class Scene(object):
-    def __init__(self, filename):
+    def __init__(self, filename, row_clip=None):
         # parse filenames
         self.filename_base = self.parse_filename_base(filename)
         print('Loading {}'.format(self.filename_base))
-        self.filename_scene = f'{self.filename_base}_3B_AnalyticMS_clip.tif'
+        self.filename_image = f'{self.filename_base}_3B_AnalyticMS_clip.tif'
         self.filename_metadata_xml = f'{self.filename_base}_3B_' \
                                      f'AnalyticMS_metadata_clip.xml'
         self.filename_metadata_json = f'{self.filename_base}_metadata.json'
@@ -22,12 +22,12 @@ class Scene(object):
         self.check_for_files()
 
         # load files into memory
-        self.scene_bands = {'blue': 1, 'green': 2, 'red': 3, 'nir': 4}
+        self.image_bands = {'blue': 1, 'green': 2, 'red': 3, 'nir': 4}
         self.udm2_bands = {'clear': 1, 'snow': 2, 'shadow': 3,
                            'light_haze': 4, 'heavy_haze': 5, 'cloud': 6,
                            'confidence': 7, 'unusable': 8}
-        self.scenes = self.load_scenes()
-        self.udm2 = self.load_udm2()
+        self.images = self.load_images(row_clip)
+        self.udm2 = self.load_udm2(row_clip)
         self.metadata_xml = self.load_metadata_xml()
         self.metadata_json = self.load_metadata_json()
 
@@ -35,26 +35,31 @@ class Scene(object):
         return filename.replace('_3B_AnalyticMS_clip.tif', '')
 
     def check_for_files(self):
-        for filename in [self.filename_scene, self.filename_udm2,
+        for filename in [self.filename_image, self.filename_udm2,
                          self.filename_metadata_xml,
                          self.filename_metadata_json]:
             if not os.path.exists(filename):
                 raise FileNotFoundError(f'{filename} missing')
 
-    def _load_rasters(self, filename, bands, raster_type):
+    def _load_rasters(self, filename, bands, row_clip):
         rasters = {}
         for raster_idx, band in enumerate(bands, 1):
             with rasterio.open(filename) as f:
                 rasters[band] = f.read(raster_idx).astype(float)
+                if row_clip is not None:
+                    if row_clip > 0:
+                        rasters[band] = rasters[band][row_clip:, :]
+                    else:
+                        rasters[band] = rasters[band][:row_clip, :]
         return rasters
 
-    def load_scenes(self):
-        return self._load_rasters(self.filename_scene,
-                                  self.scene_bands, 'scenes')
+    def load_images(self, row_clip=None):
+        return self._load_rasters(self.filename_image, self.image_bands,
+                                  row_clip)
 
-    def load_udm2(self):
-        return self._load_rasters(self.filename_udm2,
-                                  self.udm2_bands, 'udm2')
+    def load_udm2(self, row_clip=None):
+        return self._load_rasters(self.filename_udm2, self.udm2_bands,
+                                  row_clip)
 
     def load_metadata_xml(self):
         return minidom.parse(self.filename_metadata_xml)
@@ -72,6 +77,10 @@ class Scene(object):
     @property
     def acquired_label(self):
         return self.acquired.strftime('%Y-%m-%d')
+    
+    @property
+    def image_shape(self):
+        return self.images['red'].shape
 
     def _load_toa_reflectance_coeff(self, band_idx):
         nodes = self.metadata_xml.getElementsByTagName("ps:bandSpecificMetadata")
@@ -79,8 +88,8 @@ class Scene(object):
         return float(coeff)
 
     def _load_scene_toa_reflectance(self, scene_band):
-        band = self.scenes[scene_band]
-        band_idx = self.scene_bands[scene_band] - 1
+        band = self.images[scene_band]
+        band_idx = self.image_bands[scene_band] - 1
         coeff = self._load_toa_reflectance_coeff(band_idx)
         return band * coeff
 
@@ -91,9 +100,9 @@ class Scene(object):
             green = self._load_scene_toa_reflectance('green')
             blue = self._load_scene_toa_reflectance('blue')
         else:
-            red = self.scenes['red']
-            green = self.scenes['green']
-            blue = self.scenes['blue']
+            red = self.images['red']
+            green = self.images['green']
+            blue = self.images['blue']
         image_shape = red.shape
         rgb = np.zeros((image_shape[0], image_shape[1], 3))
         rgb[:, :, 0] = red / np.max(red)
@@ -107,8 +116,8 @@ class Scene(object):
             nir = self._load_scene_toa_reflectance('nir')
             red = self._load_scene_toa_reflectance('red')
         else:
-            nir = self.scenes['nir']
-            red = self.scenes['red']
+            nir = self.images['nir']
+            red = self.images['red']
         ndvi = (nir - red) / (nir + red)
         return np.nan_to_num(ndvi)
 
@@ -119,9 +128,9 @@ class Scene(object):
             red = self._load_scene_toa_reflectance('red')
             blue = self._load_scene_toa_reflectance('blue')
         else:
-            nir = self.scenes['nir']
-            red = self.scenes['red']
-            blue = self.scenes['blue']
+            nir = self.images['nir']
+            red = self.images['red']
+            blue = self.images['blue']
         evi = 2.5 * (nir - red) / (nir + 6 * red - 7.5 * blue + 1)
         return np.nan_to_num(evi)
 
@@ -131,8 +140,8 @@ class Scene(object):
             nir = self._load_scene_toa_reflectance('nir')
             green = self._load_scene_toa_reflectance('green')
         else:
-            nir = self.scenes['nir']
-            green = self.scenes['green']
+            nir = self.images['nir']
+            green = self.images['green']
         ndwi = (green - nir) / (green + nir)
         return np.nan_to_num(ndwi)
 
@@ -151,7 +160,7 @@ class Scene(object):
 
     def plot_scenes(self):
         fig, ax = plt.subplots(2, 2, figsize=(8, 8))
-        self._plot_bands(fig, ax, self.scenes, self.scene_bands, 'scenes')
+        self._plot_bands(fig, ax, self.images, self.image_bands, 'scenes')
 
     def plot_rgb(self):
         rgb = self.calculate_rgb()
