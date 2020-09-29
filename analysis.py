9b0a439c000d30e7a78f1868e0ba2137319e0764
Michael Medford <MichaelMedford@berkeley.edu>
2020-09-29 08:36:39 -0700

    docstrings

--- analysis.py
+++ analysis.py
@@ -1,5 +1,46 @@
 #!python
 
+"""
+Measuring the rate of change from green vegetation to bare soil
+in the scenes of this stack requires a measurement of vegetative health.
+I have opted to use the Normalized Difference Vegetation Index, or NDVI,
+to make this measurement. NDVI is an industry standard for measuring
+vegetative health, leveraging the fact that vegetation absorbs light in the
+optical spectrum (to process solar photons into cholorophyll) and reflects
+radiation in the near-infrared. NDVI is defined as:
+
+NDVI = (NIR - red) / (NIR + red)
+
+Plants that are extremely green due to chlorophyll production will be
+dark in red (due to absorption) and bright in NIR (due to reflection),
+leading to a large NDVI. Therefore a larger NDVI indicates more green
+vegatation, and a smaller NDVI indicates bare soil.
+
+Cleaning the data is required to get a pristine sample for measurement. This
+analysis script uses two masks, that are combined together for each scene:
+    1. A stack mask is created by finding pixels which are similarly `clean`
+       across all of the scenes in the stack. This is a conservative mask, as
+       there are valuable pixels in a given scene that may  be clouded out in a
+       separate scene. However this ensures that there are only good pixels in
+       the final sample.
+    2. Pixels that contain water cannot be either vegetation or bare soil and
+       are therefore contaminants to our measurement. These pixels are masked
+       by masking out pixels in each scene where the blue value is above
+       a percentile threshold. This percentile is transformed into a pixel
+       value for each scene and generated into a mask.
+
+This analysis script generates plots:
+    plot_ndvi_hist
+        Histograms of the NDVI pixels for each scene, with and without masks
+    plot_ndvi_trends
+        Trends of the NDVI evolution across the scenes.
+        Rate of change of the NDVI trends across the scenes.
+    plot_ndvi_trends_water_mask
+        Trends of the NDVI evolution across the scenes, with and without masks.
+    plot_water_mask
+        Images of the water mask for a single scene.
+"""
+
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,13 +57,28 @@ from product import Scene
 
 
 def load_scenes(remove_first_image=False):
+    """
+    Loads scenes into memory from disk. A later step builds a mask out of
+    overlapping pixels from the scenes, requiring them to be of the same
+    extent. However the first image is 15 rows short, requiring either
+    (1) removing the first image from the analysis, or (2) clipping the
+    remaining scenes by 15 rows. The default is to clip images
+    in order to keep as many epochs as possible.
+
+    :param bool remove_first_image: If True, remove the first image from
+        the stack due to its missing rows. If false, clip the remaining
+        images by 15 rows to create a match in size between all scenes.
+    :returns list Scene: List of scenes
+    """
     print('\nLoading scenes...')
     fname_arr = glob.glob('data/*AnalyticMS_clip.tif')
     fname_arr.sort()
 
+    # Remove the first scene from the list (if selected)
     if remove_first_image:
         fname_arr = fname_arr[1:]
 
+    # Load each scene into memory
     scene_arr = []
     for i, fname in enumerate(fname_arr):
         if remove_first_image:
@@ -39,6 +95,12 @@ def load_scenes(remove_first_image=False):
 
 
 def plot_images_and_masks(scene_arr):
+    """
+    Plot images and masks for each scene
+
+    :param list Scene scene_arr: List of scenes
+    :returns None:
+    """
     print('\nPlotting images and masks...')
     for scene in scene_arr:
         scene.plot_images()
@@ -46,14 +108,33 @@ def plot_images_and_masks(scene_arr):
 
 
 def calculate_stack_mask(scene_arr):
+    """
+    Creates a mask from the pixels that are marked `clear`
+    for all scenes in the stack.
+
+    :param list Scene scene_arr: List of scenes
+    :returns None:
+    """
     mask_arr = [s.udm2['clear'].astype(bool) for s in scene_arr]
     stack_mask = np.logical_and.reduce(mask_arr)
     return stack_mask
 
 
 def calculate_ndvi_stats(scene_arr, water_mask_percentile=50):
+    """
+    Calculates the NDVI median and standard deviation for each scene
+    in the stack. A stack mask (see `calculate_stack_mask`) and a water mask
+    are applied to each scene to generate a sample of pixels that are both
+    reliable and contain vegetation.
+
+    :param list Scene scene_arr: List of scenes
+    :param int water_mask_percentile: Percentile above which
+        blue pixels are masked
+    :returns None:
+    """
     mask_stack = calculate_stack_mask(scene_arr)
 
+    # Find and characterize the NDVI pixels for each scene
     ndvi_med_arr = []
     ndvi_std_arr = []
     acquired_arr = []
@@ -71,8 +152,22 @@ def calculate_ndvi_stats(scene_arr, water_mask_percentile=50):
 
 
 def calculate_ndvi_arr(scene_arr, apply_mask=True, water_mask_percentile=50):
+    """
+    Retrieve the NDVI pixels for each scene
+    in the stack. If apply_mask = True, a stack mask
+    (see `calculate_stack_mask`) and a water mask are applied to each scene
+    to generate a sample of pixels that are both reliable and contain
+    vegetation.
+
+    :param list Scene scene_arr: List of scenes
+    :param bool apply_mask: If True, apply all masks. If False, skip masks.
+    :param int water_mask_percentile: Percentile above which
+        blue pixels are masked
+    :returns None:
+    """
     mask_stack = calculate_stack_mask(scene_arr)
 
+    # Find NDVI pixels for each scene
     ndvi_arr = []
     for scene in scene_arr:
         mask_water = scene.calculate_percentile_mask('blue',
@@ -88,6 +183,12 @@ def calculate_ndvi_arr(scene_arr, apply_mask=True, water_mask_percentile=50):
 
 
 def generate_delta_days(acquired_arr):
+    """
+    Calculate the difference (in days) between subsequent observations.
+
+    :param list datetime acquired_arr: List of observations dates
+    :returns list float: List of days between subsequent observations.
+    """
     delta_days_arr = []
     for i in range(len(acquired_arr) - 1):
         date_start = acquired_arr[i]
@@ -100,6 +201,26 @@ def generate_delta_days(acquired_arr):
 
 def plot_ndvi_hist(scene_arr, remove_first_image=False,
                    water_mask_percentile=50):
+    """
+    Plot the NDVI pixels for each scene in the stack.
+
+    If remove_first_image=True, remove the first image from the stack due to
+    its missing rows. If False, clip the remaining images by 15 rows to
+    create a match in size between all scenes.
+
+    The subplots demonstrate the effect of applying a mask, creating a less
+    noisy and more reliable sample. They also show a clear progression to a
+    more vegetative state (higher NDVI) followed by a drop-off at the
+    latest dates.
+
+    :param list Scene scene_arr: List of scenes
+    :param bool remove_first_image: If True, remove the first image from
+        the stack due to its missing rows. If false, clip the remaining
+        images by 15 rows to create a match in size between all scenes.
+    :param int water_mask_percentile: Percentile above which
+        blue pixels are masked
+    :returns None:
+    """
     ndvi_arr = calculate_ndvi_arr(scene_arr,
                                   water_mask_percentile=water_mask_percentile)
     ndvi_unmasked_arr = calculate_ndvi_arr(scene_arr, apply_mask=False)
@@ -133,6 +254,26 @@ def plot_ndvi_hist(scene_arr, remove_first_image=False,
 
 def plot_ndvi_trends(scene_arr, remove_first_image=False,
                      water_mask_percentile=50):
+    """
+    Plot the NDVI trends across the entire stack, as well as the approximate
+    rate of change of the NDVI across the observations.
+
+    If remove_first_image=True, remove the first image from the stack due to
+    its missing rows. If False, clip the remaining images by 15 rows to
+    create a match in size between all scenes.
+
+    The trend shows a clear progression to a more vegetative state
+    (higher NDVI) in September, followed by a drop-off at the
+    latest date.
+
+    :param list Scene scene_arr: List of scenes
+    :param bool remove_first_image: If True, remove the first image from
+        the stack due to its missing rows. If false, clip the remaining
+        images by 15 rows to create a match in size between all scenes.
+    :param int water_mask_percentile: Percentile above which
+        blue pixels are masked
+    :returns None:
+    """
     ndvi_stats = calculate_ndvi_stats(scene_arr,
                                       water_mask_percentile=water_mask_percentile)
     acquired_arr, ndvi_med_arr, ndvi_std_arr = ndvi_stats
@@ -174,6 +315,17 @@ def plot_ndvi_trends(scene_arr, remove_first_image=False,
 
 
 def plot_ndvi_trends_water_mask(scene_arr):
+    """
+    Plot the NDVI trends across the entire stack and demonstrate the
+    effect of applying a water mask to the images. Both curves have
+    the stack mask applied.
+
+    The application of the water mask reduces the uncertainty in the
+    measurement, while maintaining the same evolution of the curve.
+
+    :param list Scene scene_arr: List of scenes
+    :returns None:
+    """
     ndvi_stats = calculate_ndvi_stats(scene_arr, water_mask_percentile=100)
     acquired_arr, ndvi_med_arr_raw, ndvi_std_arr_raw = ndvi_stats
     ndvi_stats = calculate_ndvi_stats(scene_arr, water_mask_percentile=50)
@@ -204,15 +356,38 @@ def plot_ndvi_trends_water_mask(scene_arr):
 
 
 def plot_water_mask(scene_arr, water_mask_percentile=50):
+    """
+    Plot the water mask for the first scene in the stack.
+
+    The water mask threshold is calculated by taking a percentile of the blue
+    pixels in the image. For example `mwater_mask_percentile=50` calculates
+    the 50th percentile value for a blue pixel, and
+    `calculate_percentile_mask` returns a mask that indicates which pixels
+    are above (and below) this threshold. Pixels which are above the water
+    mask threshold are masked out, while pixels below the water mask threshold
+    are retained.
+
+    The mask is successfully removing the water features in the image. There
+    are vegetative pixels that are lost as well, but the resulting sample
+    is a cleaner one that will better indicate the vegetative health of the
+    region.
+
+    :param list Scene scene_arr: List of scenes
+    :param int water_mask_percentile: Percentile above which
+        blue pixels are masked
+    :returns None:
+    """
     scene = scene_arr[0]
     blue = scene.images['blue']
     mask_water = scene.calculate_percentile_mask('blue',
                                                  water_mask_percentile)
 
+    #  Pixels below the water mask threshold are retained
     blue_below_threshold = np.zeros_like(blue)
     blue_below_threshold[:] = blue
     blue_below_threshold[mask_water] = 0
 
+    #  Pixels which are above the water mask threshold are masked out
     blue_above_threshold = np.zeros_like(blue)
     blue_above_threshold[:] = blue
     blue_above_threshold[~mask_water] = 0
@@ -235,6 +410,41 @@ def plot_water_mask(scene_arr, water_mask_percentile=50):
 
 def analyze_scenes(remove_first_image, water_mask_percentile,
                    skip_plot_image_masks):
+    """
+    Generate analysis plots that indicate the trends in vegetative health
+    across the scenes.
+
+    A later step builds a mask out of overlapping pixels from the scenes,
+    requiring them to be of the same extent. However the first image is 15 rows
+    short, requiring either (1) removing the first image from the analysis,
+    or (2) clipping the remaining scenes by 15 rows. If
+    remove_first_image=True, remove the first image from the stack due to
+    its missing rows. If False, clip the remaining images by 15 rows to
+    create a match in size between all scenes.
+
+    A mask that removes water features is calculated by thresholding a
+    percentile of the blue pixels in the image. For example
+    `water_mask_percentile=50` calculates the 50th percentile value for a
+    blue pixel, and `calculate_percentile_mask` returns a mask that indicates
+    which pixels are above (and below) this threshold. Pixels which are above
+    the water mask threshold are masked out, while pixels below the water mask
+    threshold are retained.
+
+    The mask is successfully removing the water features in the image. There
+    are vegetative pixels that are lost as well, but the resulting sample
+    is a cleaner one that will better indicate the vegetative health of the
+    region.
+
+    :param bool remove_first_image: If True, remove the first image from
+        the stack due to its missing rows. If false, clip the remaining
+        images by 15 rows to create a match in size between all scenes.
+    :param int water_mask_percentile: Percentile above which
+        blue pixels are masked
+    :param bool skip_plot_image_masks: If True, skip generating image and
+        mask plots for the scenes.
+    :returns None:
+    """
+
     scene_arr = load_scenes(remove_first_image=remove_first_image)
     if not skip_plot_image_masks:
         plot_images_and_masks(scene_arr)
@@ -258,7 +468,7 @@ def main():
                'masks for each scene. This flag skips that output.'
     parser.add_argument('--skip-plot-images-masks',
                         action='store_false', help=help_str)
-    parser.set_defaults(skip_plot_image_masks=True)
+    parser.set_defaults(skip_plot_image_masks=False)
 
     help_str = 'A stack mask is constructed by finding the "clear" pixels ' \
                'that all scenes in the set of imaages have in common. ' \
